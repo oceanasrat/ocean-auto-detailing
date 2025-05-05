3fc6e53aa31876b966c959eafbe6a775675525bf
oceanasrat <wykiyanosa@gmail.com>
2025-05-04 21:16:03 -0500

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-// Replace with your actual OpenAI API key (keep this file secure and private)
-const OPENAI_API_KEY = "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";
